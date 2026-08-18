47082467969c8b2ce81e19ca76d8a5143baad707
dmeshram <dmeshram@redhat.com>
2026-08-19 00:27:56 +0530

    Add health checkpoint

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/health")
+def health():
+    return jsonify({'status': 'ok'})
